e37d521b62eb7ce6b07d14856764a60d8e5c65a6
Vishwanathhatti <vishwanathhatti30@gmail.com>
2025-11-14 23:55:54 +0530

    fixed the run build issues for frontend

--- frontend/components/button.tsx
+++ frontend/components/button.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
-import { cva, type CVAProps } from "class-variance-authority"
+import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 
@@ -32,7 +32,7 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    CVAProps<typeof buttonVariants> {
+    VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
 
